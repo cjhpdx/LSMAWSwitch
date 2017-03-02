c96f514aae33783e289b44c1ff6875c5192507ed
Harris <harric@f45c89bd7281.ant.amazon.com>
2017-03-01 22:28:47 -0800

    Add mqtt two outside light

--- outside-light-esp8266/outside-light-esp8266.ino
+++ outside-light-esp8266/outside-light-esp8266.ino
@@ -4,15 +4,39 @@ const int baudRate = feather;
 const int piMotionInputPin = 13;
 const int piMotionOutputPin = 16;
 const int photocellInputPin = 0;
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;
+unsigned long t_time;
+char message_buff[100];
 
 // Light Sensitive Motion Activated Wifi Switch
 
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+
+//Wifi network config
+#define MQTT_SERVER "------"
+const char* ssid = "------";
+const char* password = "------";
+
+char* outsideGarageLightTopic = "/garage/outside/light1";
+void callback(char* topic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+PubSubClient client(MQTT_SERVER, 1883, callback, wifiClient);
+
 class LsMawSwitch{
 
-  public: //public otherwise error: test::test private
+  public: 
 
   String todStatus;
   String lightStatus;
+  const char* lightTopic;
+  const char* lightMessage;
+  const char* motionTopic;
+  const char* motionMessage;
+
 
   bool lightOn;
   bool daytime;
@@ -44,6 +68,8 @@ class LsMawSwitch{
     switchOffDelay = 10000;
     remoteOn = false;
     remoteOnStr = "false";
+    lightTopic = "/garage/outside/light1/confirm";
+    lightMessage = "Light Off";
   }
   
   void setNighttime(){ 
@@ -59,16 +85,30 @@ class LsMawSwitch{
   void readLedPhotocell(){
     lightDetected = (analogRead(A0));
   }
-  
+
+  void manualOn(){
+    remoteOn = true;
+    remoteOnStr = "true";
+    lightMessage = "Light On";
+  }
+
+  void manualOff(){
+    remoteOn = false;
+    remoteOnStr = "false";
+    lightMessage = "Light Off";
+  }
+
   void turnOnTheLight(){
     lightOn = true;
     lightStatus = "On";
+    lightMessage = "Light On";
     digitalWrite(piMotionOutputPin, HIGH);
   }
   
   void turnOffTheLight(){
     lightOn = false;
     lightStatus = "Off";
+    lightMessage = "Light Off";
     digitalWrite(piMotionOutputPin, LOW);
   }
   
@@ -171,16 +211,117 @@ class LsMawSwitch{
 
 LsMawSwitch sw = LsMawSwitch();
 
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  //convert topic to string to make it easier to work with
+  String topicStr = topic; 
+
+  //Print out some debugging info
+  Serial.println("Callback update.");
+  Serial.print("Topic: ");
+  Serial.println(topicStr);
+
+  if(payload[0] == '1'){
+    sw.manualOn();
+    client.publish(sw.lightTopic, sw.lightMessage);
+  }
+
+  else if (payload[0] == '0'){
+    sw.manualOff();
+    client.publish(sw.lightTopic, sw.lightMessage);
+  }
+
+}
+
+void reconnect() {
+
+  //attempt to connect to the wifi if connection is lost
+  if(WiFi.status() != WL_CONNECTED){
+    //debug printing
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    //loop while we wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    //print out some more debug once connected
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  //make sure we are connected to WIFI before attemping to reconnect to MQTT
+  if(WiFi.status() == WL_CONNECTED){
+  // Loop until we're reconnected to the MQTT server
+    while (!client.connected()) {
+      Serial.print("Attempting MQTT connection...");
+
+      // Generate client name based on MAC address and last 8 bits of microsecond counter
+      String clientName;
+      clientName += "esp8266-";
+      uint8_t mac[6];
+      WiFi.macAddress(mac);
+      clientName += macToStr(mac);
+
+      //if connected, subscribe to the topic(s) we want to be notified about
+      if (client.connect((char*) clientName.c_str())) {
+        Serial.print("\tMTQQ Connected");
+        client.subscribe(outsideGarageLightTopic);
+      }
+
+      //otherwise print failed for debugging
+      else{Serial.println("\tFailed."); abort();}
+    }
+  }
+}
+
+//generate unique name from MAC addr
+String macToStr(const uint8_t* mac){
+
+  String result;
+
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+
+    if (i < 5){
+      result += ':';
+    }
+  }
+
+  return result;
+}
+
 void setup() {
   Serial.begin(baudRate);
+  delay(100);
   pinMode(piMotionInputPin, INPUT);
   pinMode(piMotionOutputPin, OUTPUT);
   pinMode(A0, INPUT);
+
+  //start wifi subsystem
+  WiFi.begin(ssid, password);
+  //attempt to connect to the WIFI network and then connect to the MQTT server
+  reconnect();
+
+  //wait a bit before starting the main loop
+  delay(2000);
 }
 
 void loop() {
+  // reconnect if connection is lost
+  if (!client.connected() && WiFi.status() == 3) {reconnect();}
 
-  // Program loop starts here
+  // maintain MQTT connection
+  client.loop();
+
+  // MUST delay to allow ESP8266 WIFI functions to run
+  delay(10);
+  
+  // main program loop starts here
   sw.checkForMotion();
 
   if ( sw.remoteOn ) {
@@ -189,6 +330,17 @@ void loop() {
     sw.updateSwitch();
   }
   sw.statusReport();
+  
+  // publish every 5 seconds
+  if (millis() > (t_time + 5000)) {
+    String message =  String( "[" + sw.todStatus + "] " + "Light:" + sw.lightStatus );
+    message +=  String( " Timer:" + String(sw.timer, DEC) + " MovementDetected:" + sw.motionDetectedStr );
+    message +=  String( " LightDetected:" + String(sw.lightDetected, DEC) + " RemoteOn:" + sw.remoteOnStr );
+    
+    t_time = millis();
+    message.toCharArray(message_buff, message.length()+1);
+    client.publish("/garage/LsMawSwitch/output", message_buff);
+  }
 
 }
 
